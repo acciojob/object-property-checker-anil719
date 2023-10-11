1d7bef22d0bfda19fc0a6dc0460699130b4d6b36
Anil Terapalli <117853771+anil719@users.noreply.github.com>
2023-10-11 22:42:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
+  //   write your code herejbh
 	
-for(let i in sampleObject){
-   
-    if(i.hasOwnProperty(key)) return true;
-}
-return false;
+	for(let i in sampleObject){
+		   if(sampleObject.hasOwnProperty(key)) return true;
+	}
+	return false;
 }
 
-// Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+// // Do not change the code below
+// const key = prompt("Enter Key.");
+// alert(hasKey(key));
